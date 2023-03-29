605897628124b190c85875ec14d62ec1f83fc865
vickyxu22 <113194676+vickyxu22@users.noreply.github.com>
2023-03-29 01:24:20 -0400

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,12 +1,50 @@
-/* This is a stub for the Library class */
-public class Library {
+import java.util.Hashtable;
 
-    public Library() {
-      System.out.println("You have built a library: 📖");
+public class Library extends Building {
+    private Hashtable<String, Boolean> collection = new Hashtable<>();
+
+    public Library(String name, String address, int nFloors) {
+        super(name, address, nFloors);
     }
-  
-    public static void main(String[] args) {
-      new Library();
+
+    public void addTitle(String title) {
+        if (collection.containsKey(title)) {
+            throw new RuntimeException("Book already exists in inventory.");
+        }
+        collection.put(title, true);
     }
-  
-  }
+    
+    public String removeTitle(String title) {
+        if (!collection.containsKey(title)) {
+            throw new RuntimeException("Book not found in inventory.");
+        }
+        collection.remove(title);
+        return title;
+    }
+
+    public void checkOut(String title) {
+      if (collection.containsKey(title) && collection.get(title)) {
+          collection.put(title, false);
+      }
+  }  
+
+    public void returnBook(String title) {
+        collection.put(title, true);
+    }
+
+    public boolean containsTitle(String title) {
+        return collection.containsKey(title);
+    }
+
+    public boolean isAvailable(String title) {
+        return collection.containsKey(title) && collection.get(title);
+    }
+
+    public void printCollection() {
+        System.out.println("Library Collection:");
+        for (String title : collection.keySet()) {
+            System.out.println(title + " - " + (collection.get(title) ? "Available" : "Checked out"));
+        }
+    }
+
+}
