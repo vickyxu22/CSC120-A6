58e08488a9cad41c03d44e72b94340af2e7f1ede
vickyxu22 <113194676+vickyxu22@users.noreply.github.com>
2023-03-29 01:24:37 -0400

    Update House.java

--- House.java
+++ House.java
@@ -1,12 +1,76 @@
-/* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
 
-  public House() {
-    System.out.println("You have built a house: 🏠");
+/* This is a stub for the House class */
+public class House extends Building {
+
+  private ArrayList<String> residents;
+  private boolean hasDiningRoom;
+
+  public House(String name, String address, int nFloors, boolean hasDiningRoom) {
+    super(name, address, nFloors);
+    this.residents = new ArrayList<String>();
+    this.hasDiningRoom = hasDiningRoom;
   }
 
+  /** Accessor for hasDiningRoom */
+  public boolean hasDiningRoom() {
+    return this.hasDiningRoom;
+  }
+
+  /** Accessor for number of residents */
+  public int nResidents() {
+    return this.residents.size();
+  }
+
+  public void moveIn(String name) {
+    // check if this.residents contains name
+    if (this.residents.contains(name)) {
+      //   if so: throw and exception
+      throw new RuntimeException(name + " is already a resident of " + this.name);
+    }
+    // if we're good to go, add to roster
+    this.residents.add(name);
+    System.out.println(name + " has just moved into " + this.name + "! Go say hello :-)");
+  }
+
+  public String toString() {
+    String description = super.toString();
+    description += " There are currently " + this.nResidents() + " people living in this house.";
+    description += " This house ";
+    if (this.hasDiningRoom) {
+      description += "has";
+    } else {
+      description += "does not have";
+    }
+    description += " an active dining room.";
+    return description;
+  }
+
+  public void moveOut(String name) {
+    // check if this.residents don't contains name
+    if (!this.residents.contains(name)) {
+      //   if so: throw and exception
+      throw new RuntimeException(name + " is not a resident of " + this.name);
+    }
+    // if we're good to go, add to roster
+    this.residents.remove(name);
+    System.out.println(name + " has just moved out " + this.name);
+  }
+
+  public boolean isResident(String person) {
+    return residents.contains(person);
+  } 
   public static void main(String[] args) {
-    new House();
+    House morrow = new House("Morrow", "The Quad", 4, false);
+    System.out.println(morrow);
+    morrow.moveIn("Jordan");
+    System.out.println(morrow);
+    morrow.moveOut("Jordan");
+    System.out.println(morrow);
+    House king = new House("King", "The Quad", 3, true);
+    king.moveIn("Jordan");
+    king.moveOut("Jordan");
+    System.out.println(king);
   }
 
 }
