11d7f0b7c48bb0f8caa781a67fa86f90056d9625
vickyxu22 <113194676+vickyxu22@users.noreply.github.com>
2023-03-29 01:24:51 -0400

    Update Cafe.java

--- Cafe.java
+++ Cafe.java
@@ -1,12 +1,35 @@
-/* This is a stub for the Cafe class */
-public class Cafe {
+public class Cafe extends Building {
+    private int nCoffeeOunces;
+    private int nSugarPackets;
+    private int nCreams;
+    private int nCups;
 
-    public Cafe() {
-        System.out.println("You have built a cafe: ☕");
+    public Cafe(String name, String address, int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups) {
+        super(name, address, nCoffeeOunces);
+        this.nCoffeeOunces = nCoffeeOunces;
+        this.nSugarPackets = nSugarPackets;
+        this.nCreams = nCreams;
+        this.nCups = nCups;
     }
-    
-    public static void main(String[] args) {
-        new Cafe();
+
+    public void sellCoffee(int size, int nSugarPackets, int nCreams) {
+        if (size <= nCoffeeOunces && nSugarPackets <= this.nSugarPackets && nCreams <= this.nCreams && nCups > 0) {
+            this.nCoffeeOunces -= size;
+            this.nSugarPackets -= nSugarPackets;
+            this.nCreams -= nCreams;
+            this.nCups--;
+            System.out.println("Sold " + size + " ounces of coffee with " + nSugarPackets + " sugar packet(s) and " + nCreams + " splash(es) of cream.");
+        } else {
+            restock(nCoffeeOunces, nSugarPackets, nCreams, nCups);
+            System.out.println("Sorry, we're out of stock!");
+        }
+    }
+
+    private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups) {
+        this.nCoffeeOunces += nCoffeeOunces;
+        this.nSugarPackets += nSugarPackets;
+        this.nCreams += nCreams;
+        this.nCups += nCups;
     }
     
 }
